fb9cf0a4553ff6bf8d7f694b88c5c6991e92f922
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-05-29 16:40:09 +0100

    Added new react components

---
+++ src/food.jsx
@@ -0,0 +1,19 @@
+function Food() {
+  return <p>I love a good burger, yum yum.</p>;
+}
+
+function SpecialTest() {
+  return (
+    <div>
+      <h1>Test title</h1>
+      <svg>
+        <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+      </svg>
+      <form>
+        <input type="text" />
+      </form>
+    </div>
+  );
+}
+
+export { Food , SpecialTest};
---
+++ src/greeting.jsx
@@ -0,0 +1,5 @@
+function Greeting() {
+  return <h1>&quot;I cannot allow you to do that!&quot;</h1>;
+}
+
+export default Greeting;
